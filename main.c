42e48ab5267e8fc2ba6ebf8716061b814cc6db65
raychayut17 <ray.daroonpat.2020@gmail.com>
2020-10-16 13:34:27 +0700

    Can now pass struct to the key_event function and detect whether it matches the example sentence or not

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 # include <string.h>
 # include <gtk-2.0/gtk/gtk.h>
 
-typedef struct {
+typedef struct STR_OPS{
 	int strLen;
 	int curStrIndex;
+	gpointer label;
 	char str[10];
-} STR_OPS;
+}STR_OPS;
 
 void end_program(GtkWidget *wid, gpointer ptr){
 	gtk_main_quit();
@@ -14,31 +15,30 @@ void end_program(GtkWidget *wid, gpointer ptr){
 
 static gboolean key_event(GtkWidget *wid, 
 							GdkEventKey *event, 
-							gpointer label,
-							STR_OPS d){
+							gpointer user_data){
+	struct STR_OPS *d = user_data;
 	char key = event->keyval;
 	char temp[2];
 	sprintf(temp, "%c", key);
 	if(key > 31 && key < 127){
 		g_printerr("%c %d\n", key, key);
-		gtk_label_set_text(GTK_LABEL (label), temp);
+		gtk_label_set_text(GTK_LABEL (d->label), temp);
 		
-		if(d.str[d.curStrIndex] == key){
+		if(d->str[d->curStrIndex] == key){
 			g_printerr("Correct\n");
-			if(d.curStrIndex < d.strLen-1)
-				d.curStrIndex++;
+			if(d->curStrIndex < d->strLen-1)
+				d->curStrIndex = d->curStrIndex+1;
 		}
 	}
 	return FALSE;
 }
 
 int main(int argc, char *argv[]){
-	STR_OPS data;
+	struct STR_OPS data;
 	data.curStrIndex = 0;
 	strcpy(data.str, "Test Text");
 	data.strLen = strlen(data.str);
 	g_printerr("%d\n", data.strLen);
-	// FILE *f = fopen("./exampleText.txt");	
 	
 	gtk_init(&argc, &argv);
 	
@@ -47,8 +47,10 @@ int main(int argc, char *argv[]){
 	GtkWidget *text = gtk_label_new(data.str);
 	GtkWidget *typed = gtk_label_new(" ");
 	
+	data.label = typed;
+	
 	g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
-	g_signal_connect(win, "key-press-event", G_CALLBACK(key_event), typed, data); //  TODO: Fix Error
+	g_signal_connect(win, "key-press-event", G_CALLBACK(key_event), &data);
 	
 	gtk_table_attach_defaults(GTK_TABLE (tbl), text, 0, 2, 0, 1);
 	gtk_table_attach_defaults(GTK_TABLE (tbl), typed, 0, 2, 1, 2);
