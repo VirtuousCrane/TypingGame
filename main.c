37dec555a9cb3200bcc09a821bc09ac4749e4ca5
AuspiciousCrane <ray.daroonpat.2020@gmail.com>
2020-11-22 17:21:57 +0700

    Added the file browsing button. Completed all the main functionalities.

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 # include <time.h>
 # include <gtk-3.0/gtk/gtk.h>
 
+/*
+gcc main.c -o main $(pkg-config --cflags --libs gtk+-3.0)
+*/
+
 int correct = 0;
 int incorrect = 0;
 int word = 0;
@@ -19,52 +23,30 @@ typedef struct STR_OPS{
 	gpointer text;
 	gpointer correctLabel;
 	gpointer incorrectLabel;
+	gpointer win;
 }STR_OPS;
-
 
 void end_program(GtkWidget *wid, gpointer ptr);
 void correctIncrease(GtkWidget *wid, gpointer ptr);
 void incorrectIncrease(GtkWidget *wid, gpointer ptr);
+void browseFile(GtkWidget *wid, GdkEventKey *event, STR_OPS *user_data);
 void read_words (STR_OPS *user_data);
 void calWPM(long int timeElapsed);
 void calWpmAndExit(GtkWidget *wid, GdkEventKey *event, STR_OPS *user_data);
+void reset(STR_OPS *user_data, char fileName[]);
 static gboolean keyCallback(GtkWidget *wid, GdkEventKey *event, gpointer user_data);
+static void load_css();
 
 
 int main(int argc, char *argv[]){
-	FILE *fp;
-	const gchar *cssPath = "./styles.css";
-	fp = fopen("./exampleText.txt", "rb");
-
 	struct STR_OPS data;
-	data.curStrIndex = 0;
-	data.filePath = fp;
-	data.startTime = time(0);
+	FILE *fp;
+	fp = fopen("./exampleText.txt", "rb");
 
 	gtk_init(&argc, &argv);
 
 	GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	GtkCssProvider *cssProvider = gtk_css_provider_new();
-	GFile *cssFile = g_file_new_for_path(cssPath);
-	GError *error = NULL;
-	gtk_css_provider_load_from_file(cssProvider, cssFile, &error);
-	if(error){
-		g_warning("%s", error->message);
-		g_clear_error(&error);
-	}
-	g_object_unref (cssFile);
-
-	/*
-	GtkCssProvider *cssProvider = gtk_css_provider_new();
-	gtk_css_provider_load_from_path(cssProvider, "./styles.css", NULL);
-
-	GtkStyleContext *context;
-    context = gtk_widget_get_style_context(win);
-	gtk_style_context_add_provider (context,
-	                                GTK_STYLE_PROVIDER(cssProvider),
-	                                GTK_STYLE_PROVIDER_PRIORITY_USER);
-	*/
-
+	load_css();
 
 	GtkWidget *tbl = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 	GtkWidget *tbl_1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
@@ -77,25 +59,32 @@ int main(int argc, char *argv[]){
 	GtkWidget *btn = gtk_button_new_with_label("Yametekudastop");
 	GtkWidget *correct = gtk_label_new("Correct: 0");
 	GtkWidget *wrong = gtk_label_new("Incorrect: 0");
+	GtkWidget *fileChooser = gtk_button_new_with_label("Burowsu");
 
 	data.text = text;
 	data.label = typed;
 	data.correctLabel = correct;
 	data.incorrectLabel = wrong;
+	data.curStrIndex = 0;
+	data.filePath = fp;
+	data.startTime = time(0);
+	data.win = win;
 
 	read_words(&data);
 	data.strLen = strlen(data.str);
 
 	gtk_window_set_title(GTK_WINDOW (win), "Typing Game");
-	gtk_window_set_default_size(GTK_WINDOW (win), 1100, 100);
+	gtk_window_set_default_size(GTK_WINDOW (win), 1200, 100);
 	gtk_container_set_border_width(GTK_CONTAINER (tbl), 10);
 
 	g_signal_connect(btn, "button_release_event", G_CALLBACK(calWpmAndExit), &data);
+	g_signal_connect(fileChooser, "button_release_event", G_CALLBACK(browseFile), &data);
 	g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
 	g_signal_connect(win, "key-press-event", G_CALLBACK(keyCallback), &data);
 
 	gtk_widget_set_name(text, "exampleText");
 	gtk_widget_set_name(typed, "typedText");
+	gtk_widget_set_name(tbl_2, "typeBox");
 
 	gtk_box_pack_start(GTK_BOX (tbl), tbl_1, TRUE, TRUE, 10);
 	gtk_box_pack_start(GTK_BOX (tbl), tbl_2, TRUE, TRUE, 10);
@@ -106,9 +95,8 @@ int main(int argc, char *argv[]){
 	gtk_box_pack_start(GTK_BOX (tbl_2), typed, TRUE, TRUE, 10);
 	gtk_box_pack_end(GTK_BOX (tbl_3), correct, TRUE, TRUE, 10);
 	gtk_box_pack_end(GTK_BOX (tbl_3), wrong, TRUE, TRUE, 10);
+	gtk_box_pack_start(GTK_BOX (tbl_4), fileChooser, TRUE, TRUE, 10);
 	gtk_box_pack_end(GTK_BOX (tbl_4), btn, TRUE, TRUE, 10);
-
-
 
 	gtk_container_add(GTK_CONTAINER (win), tbl);
 
@@ -129,6 +117,26 @@ void correctIncrease(GtkWidget *wid, gpointer ptr){
 	gtk_label_set_text(GTK_LABEL (ptr), buffer);
 }
 
+void browseFile(GtkWidget *wid, GdkEventKey *event, STR_OPS *user_data){
+	GtkWidget *fileDialog = gtk_file_chooser_dialog_new("Open File",
+					                                      GTK_WINDOW(user_data->win),
+					                                      GTK_FILE_CHOOSER_ACTION_OPEN,
+					                                      "_Cancel",
+					                                      GTK_RESPONSE_CANCEL,
+					                                      "_Open",
+					                                      GTK_RESPONSE_ACCEPT,
+					                                      NULL);
+	gint res = gtk_dialog_run (GTK_DIALOG (fileDialog));
+	if (res == GTK_RESPONSE_ACCEPT){
+			char *filename;
+			GtkFileChooser *chooser = GTK_FILE_CHOOSER (fileDialog);
+			filename = gtk_file_chooser_get_filename (chooser);
+			reset(user_data, filename);
+			g_free(filename);
+		}
+	gtk_widget_destroy(fileDialog);
+}
+
 void incorrectIncrease(GtkWidget *wid, gpointer ptr){
 	char buffer[30];
 	incorrect++;
@@ -145,10 +153,9 @@ void read_words (STR_OPS *user_data) {
     if (fgets(buffer, 127, user_data->filePath) != NULL) {
         bufferLen = strlen(buffer);
 
-		if((strlen(buffer) == 1 || strlen(buffer) == 2) &&
-			(buffer[0] <= 31 || buffer[1] >= 127))
+		if(bufferLen == 0 ||(bufferLen == 1 && (buffer[0] <= 31 || buffer[0] >= 127))){
 			read_words(user_data);
-    	else if(isspace(buffer[bufferLen-1])){
+		}else if(isspace(buffer[bufferLen-1])){
     		buffer[bufferLen-1] = '\0';
     		for(int k=bufferLen; k>-1; k--){
     			if(isspace(buffer[k]))
@@ -160,12 +167,28 @@ void read_words (STR_OPS *user_data) {
 
 		    strcpy(user_data->str, buffer);
 		    user_data->str[user_data->strLen]='\0';
+			/*
+			For replacing space
+			for(int i=0; i<=bufferLen; i++){
+				if(isspace(buffer[i])){
+					buffer[i] = '>';
+				}
+			}
+			*/
 		    gtk_label_set_text(GTK_LABEL (user_data->text), buffer);
     	}else{
     		user_data->strLen = strlen(buffer);
 
 		    strcpy(user_data->str, buffer);
 		    user_data->str[user_data->strLen]='\0';
+			/*
+			For replacing space
+			for(int i=0; i<=bufferLen; i++){
+				if(isspace(buffer[i])){
+					buffer[i] = '>';
+				}
+			}
+			*/
 		    gtk_label_set_text(GTK_LABEL (user_data->text), buffer);
     	}
     }else{
@@ -218,7 +241,7 @@ static gboolean keyCallback(GtkWidget *wid, GdkEventKey *event, gpointer user_da
 			correctIncrease(wid, d->correctLabel);
 			d->typedStr[d->curStrIndex] = key;
 			d->typedStr[d->curStrIndex + 1] = '\0';
-			gtk_label_set_text(GTK_LABEL (d->label), d->typedStr);
+			gtk_label_set_text(GTK_LABEL (d->label), strcat(d->typedStr, "█"));
 
 			if (key == ' ')
 				word++;
@@ -236,4 +259,40 @@ static gboolean keyCallback(GtkWidget *wid, GdkEventKey *event, gpointer user_da
 		}
 	}
 	return FALSE;
+}
+
+static void load_css(){
+	GtkCssProvider *cssProvider;
+	GdkDisplay *display;
+	GdkScreen *screen;
+
+	const gchar *cssFile = "styles.css";
+	GFile *f = g_file_new_for_path(cssFile);
+	GError *error = 0;
+
+	cssProvider = gtk_css_provider_new();
+	display = gdk_display_get_default();
+	screen = gdk_display_get_default_screen(display);
+
+	gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	gtk_css_provider_load_from_file(cssProvider, f, &error);
+
+	g_object_unref(cssProvider);
+}
+
+void reset(STR_OPS *user_data, char fileName[]){
+	fclose(user_data->filePath);
+	FILE *fp;
+	fp = fopen(fileName, "rb");
+
+	user_data->filePath = fp;
+	correct = 0;
+	incorrect = 0;
+	user_data->curStrIndex = 0;
+	user_data->startTime  = time(0);
+	strcpy(user_data->typedStr, "");
+
+	gtk_label_set_text(GTK_LABEL (user_data->label), user_data->typedStr);
+
+	read_words(user_data);
 }
